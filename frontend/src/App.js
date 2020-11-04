5c337a58360fda584de39a017838eaf8c5ed9e45
chienvu94 <chienvv@massgenie.com>
2020-11-04 15:04:14 +0700

    2. Install React Boostrap, Fontawesome Create Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
 
 const App = () => {
   return (
     <div>
+      <Header />
+      <main>
+      <Container>
+         
+      </Container>
       
+      </main>
+      <Footer />
     </div>
   )
 }
---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className="py-3 text-center">
+                        Copy right &copy; 2020 - All right serve
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer
