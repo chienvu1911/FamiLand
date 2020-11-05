33f8f365a461cae5643e854a70d17c00adeffc86
chienvu94 <chienvv@massgenie.com>
2020-11-05 17:45:32 +0700

    9. Install Nodemon to run concurrency 2 project

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const products = require('./dataSample/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './dataSample/products.js'
 
+dotenv.config()
 const app = express()
 
 app.get('/', (req, res) => {
@@ -16,4 +18,9 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5005, console.log('Server running on port 5005'))
+const PORT = process.env.PORT || 5005
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
--- frontend/src/components/pages/ProductPage.js
+++ frontend/src/components/pages/ProductPage.js
@@ -14,7 +14,7 @@ const ProductPage = ({ match }) => {
     };
 
     fetchProduct();
-  }, []);
+  }, [match]);
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
